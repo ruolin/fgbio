4b2d06a20c8c13f9d9ea62b8314af48ca41e0377
Nils Homer <nilshomer@gmail.com>
2016-07-28 15:15:29 -0400

    Adding ByIntervalListVariantContextIterator.
    ByIntervalListVariantContextIterator iterates through variants that
    overlap a given set of intervals.

---
+++ src/main/scala/com/fulcrumgenomics/vcf/ByIntervalListVariantContextIterator.scala
@@ -0,0 +1,180 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016 Fulcrum Genomics LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+package com.fulcrumgenomics.vcf
+
+import java.util.NoSuchElementException
+
+import htsjdk.samtools.SAMSequenceDictionary
+import htsjdk.samtools.util._
+import htsjdk.variant.variantcontext.VariantContext
+import htsjdk.variant.vcf.VCFFileReader
+import com.fulcrumgenomics.FgBioDef._
+
+import scala.annotation.tailrec
+
+object ByIntervalListVariantContextIterator {
+
+  /**
+    * Creates an iterator over variant contexts that overlap any interval in an interval list.
+    *
+    * All variants will be read in and compared to the intervals.
+    */
+  def apply(iterator: Iterator[VariantContext],
+            intervalList: IntervalList,
+            dict: SAMSequenceDictionary): Iterator[VariantContext] = {
+    new OverlapDetectionVariantContextIterator(iterator, intervalList, dict)
+  }
+
+  /**
+    * Creates an iterator over variant contexts that overlap any interval in an interval list.
+    *
+    * The interval lists should be sorted and uniqued.
+    *
+    * The VCF will be queried when moving to the next variant context, and so may be quite slow
+    * if we jump around the VCF a lot.
+    */
+  def apply(reader: VCFFileReader,
+            intervalList: IntervalList): Iterator[VariantContext] = {
+    new IndexQueryVariantContextIterator(reader, intervalList)
+  }
+}
+
+private class OverlapDetectionVariantContextIterator(val iterator: Iterator[VariantContext],
+                                                     val intervalList: IntervalList,
+                                                     val dict: SAMSequenceDictionary)
+  extends Iterator[VariantContext] {
+  import scala.collection.JavaConversions.asScalaIterator
+
+  require(dict != null)
+  private val intervals = intervalList.iterator().buffered
+  private var nextVariantContext: Option[VariantContext] = None
+
+  this.advance()
+
+  def hasNext(): Boolean = this.nextVariantContext.isDefined
+
+  def next(): VariantContext = {
+    this.nextVariantContext match {
+      case None      => throw new NoSuchElementException("Called next when hasNext is false")
+      case Some(ctx) => yieldAndThen(ctx) { this.nextVariantContext = None; this.advance() }
+    }
+  }
+
+  def remove(): Unit = throw new UnsupportedOperationException
+
+  private def contigIdx(locatable: Locatable): Int = dict.getSequenceIndex(locatable.getContig)
+
+  private def coordLessThan(left: Locatable, right: Locatable): Boolean = {
+    val leftContigIdx = contigIdx(left)
+    val rightContigIdx = contigIdx(right)
+    leftContigIdx < rightContigIdx || (leftContigIdx == rightContigIdx && left.getEnd < right.getStart)
+  }
+
+  private def overlaps(left: Locatable, right: Locatable): Boolean = {
+    contigIdx(left) == contigIdx(right) && CoordMath.overlaps(left.getStart, left.getEnd, right.getStart, right.getEnd)
+  }
+
+  @tailrec
+  private def advance(): Unit = {
+    if (this.iterator.isEmpty) return
+
+    val ctx = iterator.next()
+
+    // Move to the interval that overlaps or is past this context...
+    while (intervals.hasNext && coordLessThan(intervals.head, ctx)) {
+      intervals.next()
+    }
+
+    if (intervals.isEmpty) { // no more intervals
+      Unit
+    }
+    else if (overlaps(ctx, intervals.head)) { // overlaps!!!
+      nextVariantContext = Some(ctx)
+    }
+    else if (iterator.isEmpty) { // no more variants
+      Unit
+    }
+    else { // move to the next context
+      this.advance()
+    }
+  }
+}
+
+/** NB: if a variant overlaps multiple intervals, only returns it once. */
+private class IndexQueryVariantContextIterator(private val reader: VCFFileReader, intervalList: IntervalList)
+  extends Iterator[VariantContext] {
+  private var iterator: Option[Iterator[VariantContext]] = None
+  private val intervals = intervalList.iterator()
+  private var previousInterval: Option[Interval] = None
+
+  this.advance()
+
+  def hasNext(): Boolean = {
+    this.iterator.exists(_.hasNext)
+  }
+
+  def next(): VariantContext = {
+    this.iterator match {
+      case None => throw new NoSuchElementException("Called next when hasNext is false")
+      case Some(iter) => yieldAndThen(iter.next()) { advance() }
+    }
+  }
+
+  def remove(): Unit = throw new UnsupportedOperationException
+
+  private def overlapsInterval(ctx: VariantContext, interval: Interval): Boolean = {
+    if (!ctx.getContig.equals(interval.getContig)) false // different contig
+    else if (interval.getStart <= ctx.getStart && ctx.getStart <= interval.getEnd) true // start falls within this interval, count it
+    else if (ctx.getStart < interval.getStart && interval.getEnd <= ctx.getEnd) true // the variant encloses the interval
+    else false
+  }
+
+  private def advance(): Unit = {
+    while (!this.iterator.exists(_.hasNext) && this.intervals.hasNext) {
+      val interval = this.intervals.next()
+
+      // Validate sort order of the intervals
+      previousInterval.foreach { lastInterval =>
+        val lastIntervalIdx = intervalList.getHeader.getSequenceIndex(lastInterval.getContig)
+        val intervalIdx     = intervalList.getHeader.getSequenceIndex(interval.getContig)
+        if (intervalIdx < lastIntervalIdx || (lastIntervalIdx == intervalIdx && interval.getStart <= lastInterval.getEnd)) {
+          throw new IllegalStateException(s"Intervals are out of order: '$lastInterval' and '$lastInterval'")
+        }
+      }
+
+      val lastInterval = previousInterval
+      previousInterval = Some(interval)
+      import scala.collection.JavaConversions.asScalaIterator
+
+      // NB: for variants that span an indel, make sure it was not output in the previous interval
+      val iter = this.reader.query(interval.getContig, interval.getStart, interval.getEnd)
+        .filter { ctx => overlapsInterval(ctx, interval) }
+        .filter { ctx => lastInterval.forall { interval => !overlapsInterval(ctx, interval) } }
+
+      this.iterator = Some(iter)
+    }
+  }
+}
---
+++ src/test/scala/com/fulcrumgenomics/vcf/ByIntervalListVariantContextIteratorTest.scala
@@ -0,0 +1,177 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016 Fulcrum Genomics LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.fulcrumgenomics.vcf
+
+import com.fulcrumgenomics.testing.{UnitSpec, VariantContextSetBuilder}
+import htsjdk.samtools.SAMFileHeader
+import htsjdk.samtools.util.{Interval, IntervalList}
+import htsjdk.variant.variantcontext.VariantContext
+import htsjdk.variant.vcf.VCFFileReader
+
+/**
+  * Tests for ByIntervalListVariantContextIterator.
+  */
+class ByIntervalListVariantContextIteratorTest extends UnitSpec {
+
+  private val dict = new VariantContextSetBuilder().header.getSequenceDictionary
+
+  private def emtpyIntervalList(): IntervalList = {
+    val header = new SAMFileHeader
+    header.setSequenceDictionary(this.dict)
+    new IntervalList(header)
+  }
+
+  private def toIterator(reader: VCFFileReader,
+                         intervalList: IntervalList,
+                         useIndex: Boolean): Iterator[VariantContext] = {
+    if (useIndex) {
+      ByIntervalListVariantContextIterator(reader=reader, intervalList=intervalList)
+    }
+    else {
+      import scala.collection.JavaConversions.asScalaIterator
+      ByIntervalListVariantContextIterator(iterator=reader.iterator().toIterator, intervalList=intervalList, dict=reader.getFileHeader.getSequenceDictionary)
+    }
+  }
+
+  "ByIntervalListVariantContextIterator" should "return no variants if the interval list is empty" in {
+    Stream(true, false).foreach { useIndex =>
+      val builder = new VariantContextSetBuilder().addVariant(refIdx=0, start=1, variantAlleles=List("A"), genotypeAlleles=List("A"))
+      val iterator = toIterator(reader=builder.toVcfFileReader(), intervalList=emtpyIntervalList(), useIndex=useIndex)
+      iterator shouldBe 'empty
+    }
+  }
+
+  it should "return no variants if the variants are empty" in {
+    Stream(true, false).foreach { useIndex =>
+      val builder = new VariantContextSetBuilder()
+      val intervalList = emtpyIntervalList()
+      intervalList.add(new Interval(dict.getSequence(0).getSequenceName, 1, 1000))
+      val iterator = toIterator(reader=builder.toVcfFileReader(), intervalList=emtpyIntervalList(), useIndex=useIndex)
+      iterator shouldBe 'empty
+    }
+  }
+
+  it should "return a variant context if it overlaps an interval" in {
+    Stream(true, false).foreach { useIndex =>
+      val builder = new VariantContextSetBuilder().addVariant(refIdx=0, start=500, variantAlleles=List("A"), genotypeAlleles=List("A"))
+      val intervalList = emtpyIntervalList()
+      intervalList.add(new Interval(dict.getSequence(0).getSequenceName, 1, 1000))
+      val iterator = toIterator(reader=builder.toVcfFileReader(), intervalList=intervalList, useIndex=useIndex)
+      iterator shouldBe 'nonEmpty
+      val actual = iterator.next()
+      val expected = builder.head
+      actual.getContig shouldBe expected.getContig
+      actual.getStart shouldBe expected.getStart
+      actual.getEnd shouldBe expected.getEnd
+      iterator shouldBe 'empty
+    }
+  }
+
+  it should "not return a variant context if it doesn't overlap an interval (same chromosome)" in {
+    Stream(true, false).foreach { useIndex =>
+      val builder = new VariantContextSetBuilder().addVariant(refIdx=0, start=500, variantAlleles=List("A"), genotypeAlleles=List("A"))
+      val intervalList = emtpyIntervalList()
+      intervalList.add(new Interval(dict.getSequence(0).getSequenceName, 750, 1000))
+      val iterator = toIterator(reader=builder.toVcfFileReader(), intervalList=intervalList, useIndex=useIndex)
+      iterator shouldBe 'empty
+    }
+  }
+
+  it should "not return a variant context if it doesn't overlap an interval (different chromosome)" in {
+    Stream(true, false).foreach { useIndex =>
+      val builder = new VariantContextSetBuilder().addVariant(refIdx=0, start=500, variantAlleles=List("A"), genotypeAlleles=List("A"))
+      val intervalList = emtpyIntervalList()
+      intervalList.add(new Interval(dict.getSequence(1).getSequenceName, 1, 1000))
+      val iterator = toIterator(reader=builder.toVcfFileReader(), intervalList=intervalList, useIndex=useIndex)
+      iterator shouldBe 'empty
+    }
+  }
+
+  it should "throw an exception when next() is call but hasNext() is false" in {
+    Stream(true, false).foreach { useIndex =>
+      val builder = new VariantContextSetBuilder().addVariant(refIdx=0, start=1, variantAlleles=List("A"), genotypeAlleles=List("A"))
+      val iterator = toIterator(reader=builder.toVcfFileReader(), intervalList=emtpyIntervalList(), useIndex=useIndex)
+      iterator.hasNext shouldBe false
+      an[Exception] should be thrownBy iterator.next()
+    }
+  }
+
+  it should "return a variant context if it encloses an interval" in {
+    Stream(true, false).foreach { useIndex =>
+      val builder = new VariantContextSetBuilder().addVariant(refIdx=0, start=495, variantAlleles=List("AAAAA", "A"), genotypeAlleles=List("A"))
+      val intervalList = emtpyIntervalList()
+      intervalList.add(new Interval(dict.getSequence(0).getSequenceName, 496, 496))
+      val iterator = toIterator(reader=builder.toVcfFileReader(), intervalList=intervalList, useIndex=useIndex)
+      iterator shouldBe 'nonEmpty
+      val actual = iterator.next()
+      val expected = builder.head
+      actual.getContig shouldBe expected.getContig
+      actual.getStart shouldBe expected.getStart
+      actual.getEnd shouldBe expected.getEnd
+      iterator shouldBe 'empty
+    }
+  }
+
+  it should "return a variant context only once if it overlaps multiple intervals" in {
+    Stream(true, false).foreach { useIndex =>
+      val builder = new VariantContextSetBuilder().addVariant(refIdx=0, start=495, variantAlleles=List("AAAAA", "A"), genotypeAlleles=List("A"))
+      val intervalList = emtpyIntervalList()
+      intervalList.add(new Interval(dict.getSequence(0).getSequenceName, 496, 496))
+      intervalList.add(new Interval(dict.getSequence(0).getSequenceName, 500, 500))
+      val iterator = toIterator(reader=builder.toVcfFileReader(), intervalList=intervalList, useIndex=useIndex)
+      iterator shouldBe 'nonEmpty
+      val actual = iterator.next()
+      val expected = builder.head
+      actual.getContig shouldBe expected.getContig
+      actual.getStart shouldBe expected.getStart
+      actual.getEnd shouldBe expected.getEnd
+      iterator shouldBe 'empty
+    }
+  }
+
+  it should "throw an exception when intervals are given out of order when using the VCF index" in {
+    val builder = new VariantContextSetBuilder()
+      .addVariant(refIdx=0, start=495, variantAlleles=List("AAAAA", "A"), genotypeAlleles=List("A"))
+      .addVariant(refIdx=0, start=595, variantAlleles=List("AAAAA", "A"), genotypeAlleles=List("A"))
+    val intervalList = emtpyIntervalList()
+    intervalList.add(new Interval(dict.getSequence(0).getSequenceName, 494, 500))
+    intervalList.add(new Interval(dict.getSequence(0).getSequenceName, 500, 500))
+    val iterator = toIterator(reader=builder.toVcfFileReader(), intervalList=intervalList, useIndex=true)
+    // OK, since we are overlapping the first interval
+    iterator shouldBe 'nonEmpty
+    // NOK, since the intervals were overlapping when we pre-fetch the second variant context
+    an[Exception] should be thrownBy iterator.next()
+  }
+
+  it should "ignore a variant context if does not overlap an interval" in {
+    Stream(true, false).foreach { useIndex =>
+      val builder = new VariantContextSetBuilder().addVariant(refIdx=0, start=495, variantAlleles=List("A", "C"), genotypeAlleles=List("C"))
+      val intervalList = emtpyIntervalList()
+      intervalList.add(new Interval(dict.getSequence(0).getSequenceName, 500, 500))
+      val iterator = toIterator(reader=builder.toVcfFileReader(), intervalList=intervalList, useIndex=useIndex)
+      iterator shouldBe 'empty
+    }
+  }
+}
