213b180392a05aeaa83271d25b41a619c9161004
Tim Fennell <tfenne@tfenne.com>
2016-08-31 13:06:16 -0400

    New tool to assist in reviewing reads that contributed to variant calls from consensus reads.

---
+++ src/main/scala/com/fulcrumgenomics/umi/ReviewConsensusVariants.scala
@@ -0,0 +1,150 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016 Fulcrum Genomics LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.fulcrumgenomics.umi
+
+import java.nio.file.Path
+
+import com.fulcrumgenomics.FgBioDef._
+import com.fulcrumgenomics.cmdline.{ClpGroups, FgBioTool}
+import com.fulcrumgenomics.util.Io
+import dagr.commons.util.LazyLogging
+import dagr.sopt.{arg, clp}
+import htsjdk.samtools.reference.{ReferenceSequenceFile, ReferenceSequenceFileFactory}
+import htsjdk.samtools.util.IOUtil.{VCF_EXTENSIONS => VcfExtensions}
+import htsjdk.samtools.util.{IntervalList, SequenceUtil}
+import htsjdk.samtools.{QueryInterval, SAMFileWriterFactory, SAMRecord, SamReaderFactory}
+import htsjdk.variant.vcf.VCFFileReader
+
+import scala.collection.JavaConversions.{asScalaIterator, iterableAsScalaIterable}
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+@clp(group=ClpGroups.Umi, description=
+  """
+    |Extracts data to make reviewing of variant calls from consensus reads easier. Creates
+    |a list of variant sites from the input VCF (SNPs only) or IntervalList then extracts all
+    |the consensus reads that do not contain a reference allele at the the variant sites, and
+    |all raw reads that contributed to those consensus reads.  This will include consensus
+    |reads that carry the alternate allele, a third allele, a no-call or a spanning
+    |deletion at the variant site.
+    |
+    |Reads are correlated between consensus and grouped BAMs using a molecule ID stored
+    |in an optional attribute, MI by default.  In order to support paired molecule IDs
+    |where two or more molecule IDs are related (e.g. see the Paired assignment strategy
+    |in GroupReadsByUmi) the molecule ID is truncated at the first '/' if present
+    |(e.g. "1/A" => "1" and "2" => "2").
+    |
+    |Both input BAMs must be coordinate sorted and indexed.
+    |
+    |A pair of output BAMs named <output>.consensus.bam and <output>.grouped.bam
+    |are created with the relevant reads from each input BAM.
+  """)
+class ReviewConsensusVariants
+( @arg(flag="i", doc="Input VCF or IntervalList of variant locations.") val input : FilePath,
+  @arg(flag="c", doc="BAM file of consensus reads used to call variants.")  val consensusBam : PathToBam,
+  @arg(flag="g", doc="BAM file of grouped raw reads used to build consensuses.") val groupedBam : PathToBam,
+  @arg(flag="r", doc="Reference fasta file.") val ref: PathToFasta,
+  @arg(flag="o", doc="Basename of output files to create.") val output : PathPrefix,
+  @arg(flag="t", doc="The SAM/BAM tag used to uniquely identify source molecules.") val tag: String = "MI"
+)extends FgBioTool with LazyLogging {
+  Io.assertReadable(Seq(input, consensusBam, groupedBam, ref))
+  Io.assertCanWriteFile(output)
+  private val refFile = ReferenceSequenceFileFactory.getReferenceSequenceFile(ref)
+  private val dict = refFile.getSequenceDictionary
+
+  private[umi] case class LocAndRef(chrom: String, start: Int, refBase: Char) {
+    def toQueryInterval: QueryInterval = new QueryInterval(dict.getSequenceIndex(chrom), start, start)
+  }
+
+  override def execute(): Unit = {
+    def f(ext: String): Path = output.getParent.resolve(output.getFileName + ext)
+    val consensusIn  = SamReaderFactory.make().open(consensusBam)
+    val groupedIn    = SamReaderFactory.make().open(groupedBam)
+    val factory      = new SAMFileWriterFactory().setCreateIndex(true)
+    val consensusOut = factory.makeWriter(consensusIn.getFileHeader, true, f(".consensus.bam").toFile, ref.toFile)
+    val groupedOut   = factory.makeWriter(groupedIn.getFileHeader, true, f(".grouped.bam").toFile, ref.toFile)
+
+    // Pull out the consensus reads
+    logger.info(s"Loading variants from ${input.toAbsolutePath}")
+    val variants        = loadPositions(path=input, refFile=refFile)
+    val variantsByChrom = variants.groupBy(v => v.chrom)
+    val queries         = variants.map(_.toQueryInterval).toArray
+    val sources         = mutable.HashSet[String]()
+
+    logger.info(s"Extracting consensus reads from ${consensusBam.toAbsolutePath}")
+    consensusIn.query(queries, false).filter(r => nonReferenceAtAnyVariant(r, variantsByChrom)).foreach { rec =>
+      consensusOut.addAlignment(rec)
+      Option(rec.getStringAttribute(tag)).map(mi => mi.takeWhile(_ != '/')).foreach(sources.add)
+    }
+    consensusIn.safelyClose()
+    consensusOut.close()
+
+    // And now the raw reads
+    logger.info(s"Extracting raw/grouped reads from ${groupedBam.toAbsolutePath}")
+    groupedIn.query(queries, false)
+      .filter(rec => Option(rec.getStringAttribute(tag)).map(mi => mi.takeWhile(_ != '/')).exists(sources.contains))
+      .foreach(groupedOut.addAlignment)
+    groupedIn.safelyClose()
+    groupedOut.close()
+  }
+
+  /** Loads the set of locations for review. */
+  private[umi] def loadPositions(path: FilePath, refFile: ReferenceSequenceFile): Seq[LocAndRef] = {
+    val buffer = ListBuffer[LocAndRef]()
+
+    if (VcfExtensions.exists(ext => path.getFileName.toString.endsWith(ext))) {
+      val in = new VCFFileReader(input.toFile, false)
+      in.filter(_.isSNP).foreach(v => buffer += LocAndRef(v.getContig, v.getStart, v.getReference.getBases()(0).toChar.toUpper))
+    }
+    else {
+      val list = IntervalList.fromFile(path.toFile).uniqued(false)
+      for (i <- list; pos <- i.getStart to i.getEnd) {
+        buffer += LocAndRef(i.getContig, pos, refFile.getSubsequenceAt(i.getContig, pos, pos).getBases()(0).toChar.toUpper)
+      }
+    }
+
+    buffer.toList
+  }
+
+  /**
+    * Returns true if the read contains either a variant base, a no-call, or a deletion at one or more
+    * of the variants that we are interested in.
+
+    * @param rec the SAMRecord of interest
+    * @param variantsByChromAndPos the set of variants we care about, grouped by chromosome
+    * @return true if the read is non-reference at any variant base, otherwise false
+    */
+  private def nonReferenceAtAnyVariant(rec: SAMRecord, variantsByChromAndPos: Map[String, Seq[LocAndRef]]): Boolean = {
+    !rec.getReadUnmappedFlag && variantsByChromAndPos(rec.getContig).exists { v =>
+      if (v.start >= rec.getAlignmentStart && v.start <= rec.getAlignmentEnd) {
+        val readPos = rec.getReadPositionAtReferencePosition(v.start)
+        readPos == 0 || !SequenceUtil.basesEqual(rec.getReadBases()(readPos-1), v.refBase.toByte)
+      }
+      else {
+        false
+      }
+    }
+  }
+}
---
+++ src/test/scala/com/fulcrumgenomics/umi/ReviewConsensusVariantsTest.scala
@@ -0,0 +1,195 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016 Fulcrum Genomics LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.fulcrumgenomics.umi
+
+import com.fulcrumgenomics.FgBioDef._
+import com.fulcrumgenomics.testing.{SamRecordSetBuilder, UnitSpec, VariantContextSetBuilder}
+import com.fulcrumgenomics.util.Io
+import com.fulcrumgenomics.util.NumericTypes.PhredScore
+import dagr.commons.io.PathUtil
+import htsjdk.samtools.SAMFileHeader.SortOrder
+import htsjdk.samtools.reference.{ReferenceSequenceFile, ReferenceSequenceFileFactory}
+import htsjdk.samtools.util.{Interval, IntervalList}
+import htsjdk.samtools.{SAMFileHeader, SAMFileWriterFactory, SAMRecord, SamReaderFactory}
+
+import scala.collection.JavaConversions.{asScalaIterator, iterableAsScalaIterable}
+
+object ReviewConsensusVariantsTest {
+  val Fasta : Seq[String] =
+    """
+      |>chr1
+      |AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      |AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      |>chr2
+      |CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+      |CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+    """.stripMargin.lines.toSeq
+
+  val Fai : Seq[String] =
+    """
+      |chr1    100     6       50      51
+      |chr2    100     114     50      51
+    """.stripMargin.lines.map(_.trim.replaceAll("\\s+", "\t")).filter(_.nonEmpty).toSeq
+
+  val Dict : Seq[String] =
+    """
+      |@HD    	VN:1.5 	SO:unsorted
+      |@SQ    	SN:chr1	LN:100 	M5:8adc5937e635f6c9af646f0b23560fae    	AS:n/a 	UR:n/a 	SP:n/a
+      |@SQ    	SN:chr2	LN:100 	M5:48ecc9d349522f836cadf615e370bc51    	AS:n/a 	UR:n/a 	SP:n/a
+    """.stripMargin.lines.map(_.trim.replaceAll("\\s+", "\t")).filter(_.nonEmpty).toSeq
+}
+
+
+class ReviewConsensusVariantsTest extends UnitSpec {
+  import ReviewConsensusVariantsTest._
+
+  /** Sets up a trivial reference for testing against. */
+  lazy val refFasta: PathToFasta = {
+    val ref  = makeTempFile("ref.", ".fa")
+    val fai  = ref.getParent.resolve(ref.getFileName + ".fai")
+    val dict = PathUtil.replaceExtension(ref, ".dict")
+    Seq(fai, dict).foreach(_.toFile.deleteOnExit())
+
+    Io.writeLines(ref, Fasta)
+    Io.writeLines(fai, Fai)
+    Io.writeLines(dict, Dict)
+    ref
+  }
+
+  lazy val ref: ReferenceSequenceFile = ReferenceSequenceFileFactory.getReferenceSequenceFile(refFasta)
+
+  lazy val header = {
+    val h = new SAMFileHeader
+    h.setSequenceDictionary(ref.getSequenceDictionary)
+    h
+  }
+
+  // We're going to simulate raw reads and consensuses as if there were variants at:
+  //    chr1:10
+  //    chr1:20
+  //    chr1:30
+  //    chr2:20
+  lazy val (rawBam, consensusBam) : (PathToBam, PathToBam) = {
+    val raw = new SamRecordSetBuilder(readLength=10, sortOrder=SortOrder.coordinate)
+    val con = new SamRecordSetBuilder(readLength=10, sortOrder=SortOrder.coordinate, baseQuality=45)
+    raw.header.setSequenceDictionary(ref.getSequenceDictionary)
+    con.header.setSequenceDictionary(ref.getSequenceDictionary)
+
+    // Add two raw read pairs and a consensus, with a mismatch at base 10 in the reference
+    raw.addPair(name="A1", contig=0, start1=6, start2=50, attrs=Map("MI" -> "A")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAATAAAAA" else "AAAAAAAAAA"))
+    raw.addPair(name="A2", contig=0, start1=6, start2=50, attrs=Map("MI" -> "A")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAATAAAAG" else "AAAAAAAAAA"))
+    con.addPair(name="A" , contig=0, start1=6, start2=50, attrs=Map("MI" -> "A")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAATAAAAN" else "AAAAAAAAAA"))
+
+
+    // Two consensus, only one supporting the alt, with one raw read pair each, at chr1:20
+    raw.addPair(name="B1", contig=0, start1=16, start2=50, attrs=Map("MI" -> "B")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAACAAAAA" else "AAAAAAAAAA"))
+    con.addPair(name="B" , contig=0, start1=16, start2=50, attrs=Map("MI" -> "B")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAACAAAAA" else "AAAAAAAAAA"))
+
+    raw.addPair(name="C1", contig=0, start1=17, start2=50, attrs=Map("MI" -> "C")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAAAAAAAA" else "AAAAAAAAAA"))
+    con.addPair(name="C" , contig=0, start1=17, start2=50, attrs=Map("MI" -> "C")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAAAAAAAA" else "AAAAAAAAAA"))
+
+
+    // A messy locus with consensuses with spanning deletions(D), no-calls(E), variant alleles(F) and mismatches at non-variant positions(G) at chr1:30
+    raw.addPair(name="D1", contig=0, start1=25, start2=60, cigar1="4M4D6M", attrs=Map("MI" -> "D")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAAAAAAAA" else "AAAAAAAAAA"))
+    con.addPair(name="D" , contig=0, start1=25, start2=60, cigar1="4M4D6M", attrs=Map("MI" -> "D")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAAAAAAAA" else "AAAAAAAAAA"))
+
+    raw.addPair(name="E1", contig=0, start1=26, start2=60, attrs=Map("MI" -> "E")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAANAAAAA" else "AAAAAAAAAA"))
+    con.addPair(name="E" , contig=0, start1=26, start2=60, attrs=Map("MI" -> "E")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAANAAAAA" else "AAAAAAAAAA"))
+
+    raw.addPair(name="F1", contig=0, start1=27, start2=60, attrs=Map("MI" -> "F")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAGAAAAAA" else "AAAAAAAAAA"))
+    con.addPair(name="F" , contig=0, start1=27, start2=60, attrs=Map("MI" -> "F")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAGAAAAAA" else "AAAAAAAAAA"))
+
+    raw.addPair(name="G1", contig=0, start1=28, start2=60, attrs=Map("MI" -> "G")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAAAAAGAA" else "AAAAAAAAAA"))
+    con.addPair(name="G" , contig=0, start1=28, start2=60, attrs=Map("MI" -> "G")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAAAAAGAA" else "AAAAAAAAAA"))
+
+
+    // A case where both ends of a pair overlap the variant position at chr2:20
+    raw.addPair(name="H1", contig=1, start1=15, start2=19, attrs=Map("MI" -> "H")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAAATAAAA" else "ATAAAAAAAA"))
+    con.addPair(name="H" , contig=1, start1=15, start2=19, attrs=Map("MI" -> "H")).foreach(r => r.setReadString(if (r.getFirstOfPairFlag) "AAAAATAAAA" else "ATAAAAAAAA"))
+
+
+    // Some unmapped reads at the end of the file
+    raw.addPair(name="X1", record1Unmapped = true, record2Unmapped = true, start1= -1, start2= -2, attrs=Map("MI" -> "X"))
+    raw.addPair(name="X",  record1Unmapped = true, record2Unmapped = true, start1= -1, start2= -2, attrs=Map("MI" -> "X"))
+
+    (raw.toTempFile(), con.toTempFile())
+  }
+
+  /** Returns queryname + /1 or /2 */
+  private def nameAndNumber(rec: SAMRecord): String = rec.getReadName + "/" + (if (rec.getFirstOfPairFlag) "1" else "2")
+
+  "ReviewConsensusVariants" should "create empty BAMs when given an empty interval list as input" in {
+    val outBase = makeTempFile("review_consensus.", ".out")
+    val conOut  = outBase.getParent.resolve(outBase.getFileName + ".consensus.bam")
+    val rawOut  = outBase.getParent.resolve(outBase.getFileName + ".grouped.bam")
+    val intervals = makeTempFile("empty.", ".interval_list")
+    new IntervalList(header).write(intervals.toFile)
+    new ReviewConsensusVariants(input=intervals, consensusBam=consensusBam, groupedBam=rawBam, ref=refFasta, output=outBase).execute()
+
+    conOut.toFile.exists() shouldBe true
+    rawOut.toFile.exists() shouldBe true
+
+    readBamRecs(rawOut) shouldBe 'empty
+    readBamRecs(conOut) shouldBe 'empty
+  }
+
+  "ReviewConsensusVariants" should "create empty BAMs when given an empty VCF as input" in {
+    val outBase = makeTempFile("review_consensus.", ".out")
+    val conOut  = outBase.getParent.resolve(outBase.getFileName + ".consensus.bam")
+    val rawOut  = outBase.getParent.resolve(outBase.getFileName + ".grouped.bam")
+    val intervals = makeTempFile("empty.", ".vcf")
+
+    val vcfBuilder = VariantContextSetBuilder("s1")
+    vcfBuilder.header.setSequenceDictionary(header.getSequenceDictionary)
+    vcfBuilder.write(intervals)
+    new ReviewConsensusVariants(input=intervals, consensusBam=consensusBam, groupedBam=rawBam, ref=refFasta, output=outBase).execute()
+
+    conOut.toFile.exists() shouldBe true
+    rawOut.toFile.exists() shouldBe true
+
+    readBamRecs(rawOut) shouldBe 'empty
+    readBamRecs(conOut) shouldBe 'empty
+  }
+
+  "ReviewConsensusVariants" should "create extract the right reads given a set of loci" in {
+    val outBase = makeTempFile("review_consensus.", ".out")
+    val conOut  = outBase.getParent.resolve(outBase.getFileName + ".consensus.bam")
+    val rawOut  = outBase.getParent.resolve(outBase.getFileName + ".grouped.bam")
+    val intervals = makeTempFile("empty.", ".interval_list")
+    val ilist = new IntervalList(header)
+    ilist.add(new Interval("chr1", 10, 10))
+    ilist.add(new Interval("chr1", 20, 20))
+    ilist.add(new Interval("chr1", 30, 30))
+    ilist.add(new Interval("chr2", 20, 20))
+    ilist.sorted().write(intervals.toFile)
+    new ReviewConsensusVariants(input=intervals, consensusBam=consensusBam, groupedBam=rawBam, ref=refFasta, output=outBase).execute()
+
+    conOut.toFile.exists() shouldBe true
+    rawOut.toFile.exists() shouldBe true
+
+    readBamRecs(rawOut).map(nameAndNumber) should contain theSameElementsAs Seq("A1/1", "A2/1", "B1/1", "D1/1", "E1/1", "F1/1", "H1/1", "H1/2")
+    readBamRecs(conOut).map(nameAndNumber) should contain theSameElementsAs Seq("A/1", "B/1", "D/1", "E/1", "F/1", "H/1", "H/2")
+  }
+}
