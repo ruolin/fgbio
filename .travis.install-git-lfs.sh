56ae462e865dc1d6726e1733d91c3fdd25356859
Nils Homer <nh13@users.noreply.github.com>
2017-04-05 19:44:10 -0400

    Using an authenticated remote for git lfs. (#204)

--- .travis.install-git-lfs.sh
+++ .travis.install-git-lfs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GITHUB_ACCESS_TOKEN=$1;
+
 GIT_LFS_VERSION="2.0.2"
 GIT_LFS_LINK=https://github.com/github/git-lfs/releases/download/v${GIT_LFS_VERSION}/git-lfs-linux-amd64-${GIT_LFS_VERSION}.tar.gz
 GIT_LFS="git-lfs-${GIT_LFS_VERSION}/git-lfs"
@@ -9,7 +11,7 @@ git_dir=`dirname $(which git)`
 cp $GIT_LFS $git_dir/
 
 echo "Resetting remote URL"
-git remote set-url origin "https://github.com/fulcrumgenomics/fgbio.git"
+git remote set-url origin "https://${GITHUB_ACCESS_TOKEN}@github.com/fulcrumgenomics/fgbio.git"
 
 echo "Fetching LFS files."
 $GIT_LFS install
