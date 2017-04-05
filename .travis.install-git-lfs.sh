029da283ac331a243fa108a8dff12e26025ab5af
Tim Fennell <tfenne@tfenne.com>
2017-04-05 18:17:19 -0400

    Updated version of git-lfs used on travis. (#203)
    Updated git-lfs version and install protocol.  Updated how custom R packages are built, installed and cached.

--- .travis.install-git-lfs.sh
+++ .travis.install-git-lfs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-GIT_LFS_VERSION="1.5.4"
+GIT_LFS_VERSION="2.0.2"
 GIT_LFS_LINK=https://github.com/github/git-lfs/releases/download/v${GIT_LFS_VERSION}/git-lfs-linux-amd64-${GIT_LFS_VERSION}.tar.gz
 GIT_LFS="git-lfs-${GIT_LFS_VERSION}/git-lfs"
 echo "Downloading and untarring git-lfs binary" 
 wget -qO- $GIT_LFS_LINK | tar xvz
+git_dir=`dirname $(which git)`
+cp $GIT_LFS $git_dir/
 
 echo "Resetting remote URL"
 git remote set-url origin "https://github.com/fulcrumgenomics/fgbio.git"
