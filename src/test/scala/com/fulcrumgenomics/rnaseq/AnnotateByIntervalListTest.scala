bcb92c66938e5c7359440b6eb860023f043a990c
Jay Carey <jaycarey@fulrcumgenomics.com>
2020-09-02 14:33:10 -0400

    Cleanup

--- src/test/scala/com/fulcrumgenomics/rnaseq/AnnotateByIntervalListTest.scala
+++ src/test/scala/com/fulcrumgenomics/rnaseq/AnnotateByIntervalListTest.scala
@@ -9,23 +9,22 @@ import htsjdk.samtools.util.Interval
 import org.scalatest.OptionValues
 
 class AnnotateByIntervalListTest extends UnitSpec with ErrorLogLevel with OptionValues {
-  private val (ref, refPath) = {
+  private val ref = {
     val builder = new ReferenceSetBuilder
     builder.add("chr1").add("AAAAAAAAAA", 50) // 500 bases
     builder.add("chr2").add("CCCCCCCCCC", 50) // 500 bases
 
-    val refPath = builder.toTempFile()
-    val ref = ReferenceSequenceFileFactory.getReferenceSequenceFile(refPath)
-    (ref, refPath)
+    val ref     = ReferenceSequenceFileFactory.getReferenceSequenceFile(builder.toTempFile())
+    ref
   }
 
   val dict: SAMSequenceDictionary = ref.getSequenceDictionary
 
   "AnnotateByIntervalList" should "associate template bases if the template is enclosed in an interval" in {
-    val builder = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
-    val intervalPath = makeTempFile("annotatebyinterval", "interval_list")
-    val tmpOutput = makeTempFile("annotatebyinterval_metrics", "txt")
-    val writer = IntervalListWriter(intervalPath, dict.fromSam)
+    val builder       = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
+    val intervalPath  = makeTempFile("annotatebyinterval", "interval_list")
+    val tmpOutput     = makeTempFile("annotatebyinterval_metrics", "txt")
+    val writer        = IntervalListWriter(intervalPath, dict.fromSam)
 
     writer += new Interval("chr1", 1, 350, false, "interval1")
     writer.close()
@@ -38,10 +37,10 @@ class AnnotateByIntervalListTest extends UnitSpec with ErrorLogLevel with Option
   }
 
   "AnnotateByIntervalList" should "not associate template bases if the template is not fully enclosed in an interval" in {
-    val builder = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
-    val intervalPath = makeTempFile("annotatebyinterval", "interval_list")
-    val tmpOutput = makeTempFile("annotatebyinterval_metrics", "txt")
-    val writer = IntervalListWriter(intervalPath, dict.fromSam)
+    val builder       = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
+    val intervalPath  = makeTempFile("annotatebyinterval", "interval_list")
+    val tmpOutput     = makeTempFile("annotatebyinterval_metrics", "txt")
+    val writer        = IntervalListWriter(intervalPath, dict.fromSam)
 
     writer += new Interval("chr1", 1, 200, false, "interval1")
     writer.close()
@@ -54,10 +53,10 @@ class AnnotateByIntervalListTest extends UnitSpec with ErrorLogLevel with Option
   }
 
   "AnnotateByIntervalList" should "associate bases with only enclosing intervals" in {
-    val builder = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
-    val intervalPath = makeTempFile("annotatebyinterval", "interval_list")
-    val tmpOutput = makeTempFile("annotatebyinterval_metrics", "txt")
-    val writer = IntervalListWriter(intervalPath, dict.fromSam)
+    val builder       = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
+    val intervalPath  = makeTempFile("annotatebyinterval", "interval_list")
+    val tmpOutput     = makeTempFile("annotatebyinterval_metrics", "txt")
+    val writer        = IntervalListWriter(intervalPath, dict.fromSam)
 
     writer += new Interval("chr1", 1, 500, false, "interval1")
     writer += new Interval("chr1", 152, 300, false, "interval2")
@@ -72,10 +71,10 @@ class AnnotateByIntervalListTest extends UnitSpec with ErrorLogLevel with Option
   }
 
   "AnnotateByIntervalList" should "associate bases with all enclosing intervals (duplicate counts)" in {
-    val builder = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
-    val intervalPath = makeTempFile("annotatebyinterval", "interval_list")
-    val tmpOutput = makeTempFile("annotatebyinterval_metrics", "txt")
-    val writer = IntervalListWriter(intervalPath, dict.fromSam)
+    val builder       = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
+    val intervalPath  = makeTempFile("annotatebyinterval", "interval_list")
+    val tmpOutput     = makeTempFile("annotatebyinterval_metrics", "txt")
+    val writer        = IntervalListWriter(intervalPath, dict.fromSam)
 
     writer += new Interval("chr1", 1, 500, false, "interval1")
     writer += new Interval("chr1", 10, 490, false, "interval2")
@@ -92,10 +91,10 @@ class AnnotateByIntervalListTest extends UnitSpec with ErrorLogLevel with Option
   }
 
   "AnnotateByIntervalList" should "associate bases with the smallest interval (most specific counts)" in {
-    val builder = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
-    val intervalPath = makeTempFile("annotatebyinterval", "interval_list")
-    val tmpOutput = makeTempFile("annotatebyinterval_metrics", "txt")
-    val writer = IntervalListWriter(intervalPath, dict.fromSam)
+    val builder       = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
+    val intervalPath  = makeTempFile("annotatebyinterval", "interval_list")
+    val tmpOutput     = makeTempFile("annotatebyinterval_metrics", "txt")
+    val writer        = IntervalListWriter(intervalPath, dict.fromSam)
 
     writer += new Interval("chr1", 1, 500, false, "interval1")
     writer += new Interval("chr1", 10, 490, false, "interval2")
@@ -113,10 +112,10 @@ class AnnotateByIntervalListTest extends UnitSpec with ErrorLogLevel with Option
   }
 
   "AnnotateByIntervalList" should "associate bases with the largest interval (least specific counts)" in {
-    val builder = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
-    val intervalPath = makeTempFile("annotatebyinterval", "interval_list")
-    val tmpOutput = makeTempFile("annotatebyinterval_metrics", "txt")
-    val writer = IntervalListWriter(intervalPath, dict.fromSam)
+    val builder       = new SamBuilder(readLength=50, sd=Some(dict.fromSam))
+    val intervalPath  = makeTempFile("annotatebyinterval", "interval_list")
+    val tmpOutput     = makeTempFile("annotatebyinterval_metrics", "txt")
+    val writer        = IntervalListWriter(intervalPath, dict.fromSam)
 
     writer += new Interval("chr1", 1, 500, false, "interval1")
     writer += new Interval("chr1", 10, 490, false, "interval2")
